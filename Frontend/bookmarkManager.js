4509eb01a2c7efad89737ed4f04733b463016469
kmrober3 <kmroberts@unc.edu>
2025-09-26 15:52:41 -0400

    Added textboxes for future deletion functionality

--- Frontend/bookmarkManager.js
+++ Frontend/bookmarkManager.js
@@ -11,15 +11,19 @@ class bookmarkManager {
         for (let i = 0; i < this.lS.length; i++) {    
             let path = document.createElement("a"); 
             let value = document.createElement("li");  
+            let box = document.createElement("input");
+            box.type = "checkbox"; 
+            box.id = this.lS.key(i);
 
             path.href = this.lS.key(i);
             path.textContent = this.lS.key(i); 
             path.target = "_blank"; // Fix for links, dont know why
 
-            value.id = this.lS.key(i);  
-            value.appendChild(path); 
+            //value.id = this.lS.key(i);  
+            value.append(path); 
 
-            listContent.append(value);
+            listContent.append(value); 
+            listContent.append(box); 
         }
     }  
 
@@ -29,17 +33,42 @@ class bookmarkManager {
         console.log(this.lS);
         for (let i = 0; i < this.lS.length; i++) {  
             let path = document.createElement("a"); 
-            let value = document.createElement("li"); 
+            let value = document.createElement("li");  
+            let box = document.createElement("input");
+            box.type = "checkbox"; 
+            box.id = this.lS.key(i);  
+            path.appendChild(box); 
 
             path.href = this.lS.key(i);
             path.textContent = this.lS.key(i); 
             path.target = "_blank"; 
 
-            value.id = this.lS.key(i); 
-            value.appendChild(path); 
+            //value.id = this.lS.key(i); 
+            value.append(path); 
 
-            listContent.append(value);
+            listContent.append(value); 
+            listContent.append(box);  
+
+            //console.log(path);
         }
+    } 
+
+    deleteItems() {   
+        let body = document.getElementById("urlContent");
+        body.innerHTML = ""; 
+        let arr = [];
+        for (let i = 0; i < this.lS.length; i++) {
+            arr.push(this.lS.key(i));
+        } 
+        console.log(arr);
+        for (let i = 0; i < arr.length; i++) { 
+            let boxVal = document.getElementById(arr[i]);
+            if (boxVal && boxVal.checked) { 
+                this.lS.removeItem(arr[i]);
+            }
+        }  
+        console.log("deleteItems" + this.lS);
+        this.addItem();
     }
 
     clearLS() {
@@ -59,6 +88,10 @@ document.getElementById("add").addEventListener("click", (event) => {
 document.addEventListener("DOMContentLoaded", (event) => { 
     instance.loadItems();
 })
+
+document.getElementById("remove").addEventListener("click", (event) => {
+    this.deleteItems();
+});
 
 /*document.addEventListener("DOMContentLoaded", (event) => {
     instance.clearLS();
