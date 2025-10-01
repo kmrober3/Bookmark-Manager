7e76bbd0991dfd1625665f8a49dde641e5cadf76
kmrober3 <kmroberts@unc.edu>
2025-09-30 20:35:13 -0400

    Frontend and backend fully functional

--- Frontend/bookmarkManager.js
+++ Frontend/bookmarkManager.js
@@ -4,10 +4,17 @@ class bookmarkManager {
         this.lS = localStorage; 
     }
 
-    addItem(input) { 
+    async addItem(input) { 
         let listContent = document.getElementById("urlContent");
-        listContent.innerHTML = "";      
-        this.lS.setItem(encodeURIComponent(input), input);
+        listContent.innerHTML = "";        
+
+        try {
+            const result = await fetch(`http://localhost:8080/title?url=${input}`);  
+            let val = await result.json(); 
+            this.lS.setItem(encodeURIComponent(input), val.title);
+        } catch (error) { 
+            throw new Error("FAIL");
+        };
         for (let i = 0; i < this.lS.length; i++) {     
             //Creating of html elements 
             let value = document.createElement("li");
@@ -21,9 +28,8 @@ class bookmarkManager {
             console.log(box.id);
 
             //Adding hyperlink to webpage
-            path.href = this.lS.getItem(this.lS.key(i));
-            path.textContent = this.lS.getItem(this.lS.key(i)); 
-            path.target = "_blank"; // Fix for links, dont know why
+            path.href = decodeURIComponent(this.lS.key(i));
+            path.textContent = this.lS.getItem(this.lS.key(i)) + " " + decodeURIComponent(this.lS.key(i))
 
             value.append(path); 
 
@@ -46,8 +52,8 @@ class bookmarkManager {
             box.id = this.lS.key(i);   
 
             //Adding hyperlink to webpage
-            path.href = this.lS.getItem(this.lS.key(i));
-            path.textContent = this.lS.getItem(this.lS.key(i)); 
+            path.href = decodeURIComponent(this.lS.key(i));
+            path.textContent = this.lS.getItem(this.lS.key(i)) + " " + decodeURIComponent(this.lS.key(i)); 
             path.target = "_blank";  
 
             value.append(path); 
@@ -61,16 +67,16 @@ class bookmarkManager {
         let body = document.getElementById("urlContent");
         let arr = [];
         for (let i = 0; i < this.lS.length; i++) {
-            arr.push(this.lS.getItem(this.lS.key(i)));
+            arr.push(this.lS.key(i));
         } 
         console.log(arr);
         for (let i = 0; i < arr.length; i++) {  
             console.log("HI");
-            let boxVal = document.getElementById(encodeURIComponent(arr[i])); 
+            let boxVal = document.getElementById(arr[i]); 
             console.log(boxVal);
             if (boxVal && boxVal.checked) {  
                 console.log("Please work");
-                this.lS.removeItem(encodeURIComponent(arr[i]));
+                this.lS.removeItem(arr[i]);
             }
         }   
         //console.log("deleteItems" + this.lS);   
@@ -89,8 +95,8 @@ class bookmarkManager {
             console.log(box.id);
 
             //Adding hyperlink to webpage
-            path.href = this.lS.getItem(this.lS.key(i));
-            path.textContent = this.lS.getItem(this.lS.key(i)); 
+            path.href = decodeURIComponent(this.lS.key(i));
+            path.textContent = this.lS.getItem(this.lS.key(i)) + " " + decodeURIComponent(this.lS.key(i)); 
             path.target = "_blank"; // Fix for links, dont know why
 
             value.append(path); 
@@ -116,8 +122,6 @@ document.getElementById("add").addEventListener("click", (event) => {
 }); 
 
 document.addEventListener("DOMContentLoaded", (event) => {   
-    console.log("HI");
-    console.log(instance);
     instance.loadItems();
 });
 
--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from "express";  
 import fetch from "node-fetch";           
-import { JSDOM } from "jsdom";     
+import { JSDOM } from "jsdom";  
+import cors from "cors";    
 
 
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;  
 
-app.use(express.json());
+app.use(express.json()); 
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
